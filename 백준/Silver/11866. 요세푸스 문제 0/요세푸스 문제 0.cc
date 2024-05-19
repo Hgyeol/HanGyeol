afe3062d3774ae06400bc6195a83c4c4cca00197
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-05-19 20:41:28 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,30 @@
+// Online C compiler to run
+#include<stdio.h>
+int main() {
+    int n,k, tmp,cnt=1, p_cnt;
+    scanf("%d%d", &n, &k);
+    p_cnt=n;
+    int arr[1001];
+    for(int i = 1; i <= n; i++) {
+        arr[i-1] = i;
+    }
+    printf("<");
+    while(n>0) {
+        for(int i = 0; i < n; i++) {
+            if(cnt==k) {
+                if(p_cnt>1) printf("%d, ", arr[i]);
+                else printf("%d>",arr[i]);
+                for(int j = i; j < n-1; j++) {
+                    tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+                n--;
+                i--;
+                cnt=1;
+                p_cnt--;
+            }
+            else cnt++;
+        }
+    }
+}
