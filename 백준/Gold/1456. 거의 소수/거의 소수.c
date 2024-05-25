8b15afa8caea9722a01857a1ea87e13a15b9eb97
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-05-26 01:10:58 +0900

    [Gold V] Title: 거의 소수, Time: 108 ms, Memory: 18572 KB -BaekjoonHub

---
+++ 백준/Gold/1456. 거의 소수/거의 소수.c
@@ -0,0 +1,30 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    bool arr[10000001] = {0};
+    long long int res[1000000] ={0};
+    long long int a, b;
+    int cnt=0, res_cnt=0;
+    scanf("%lld%lld", &a, &b);
+    
+    for(int i = 2; i <= 10000001; i++) {
+        if(arr[i]!=1) {
+            res[cnt]=i;
+            cnt++;
+            for(int j = 2 * i; j <= 10000001; j+=i) {
+                arr[j] = 1;
+            }
+        }
+    }
+    for(int i = 0; i < cnt; i++) {
+        for(long long int j = res[i] * res[i]; j <= b; j *= res[i]) {
+            if(j>=a) {
+                res_cnt++;
+                if(j > 1000000000000000LL/res[i]) break;
+            }
+        }
+    }
+	printf("%d", res_cnt);
+    return 0;
+}
