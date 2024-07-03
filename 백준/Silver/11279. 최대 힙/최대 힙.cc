3ef2e1cc3dabfebd84e78826c8cfbc16ccaf4841
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-07-03 21:53:26 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+    priority_queue<int> pq;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, num;
+    
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> num;
+        if(num!=0) {
+            pq.push(num);
+        }
+        else if(pq.empty()) {
+            cout << 0 << "\n";
+        }
+        else {
+            cout << pq.top() << "\n";
+            pq.pop();
+        }
+    }
+    
+    return 0;
+}
