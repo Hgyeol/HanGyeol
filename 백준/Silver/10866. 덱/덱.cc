81a56b867867faaff0bd7782d94e2552e175f53a
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-07-13 11:02:24 +0900

    [Silver IV] Title: 덱, Time: 228 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cc
@@ -0,0 +1,72 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <deque>
+using namespace std;
+int main() {
+    deque<int> dq;
+    int n, num;
+    string s;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> s;
+        //cout << s << "\n";
+        if(s.compare("push_back")==0) {
+            cin >> num;
+            dq.push_back(num);
+        }
+        else if(s.compare("push_front")==0) {
+            cin >> num;
+            dq.push_front(num);
+        }
+        else if(s.compare("pop_back")==0) {
+            if(dq.empty()==1) {
+                cout << "-1\n";
+            }
+            else {
+                cout << dq.back() << "\n";
+                dq.pop_back();
+            }
+            
+        }
+        else if(s.compare("pop_front")==0) {
+            if(dq.empty()==1) {
+                cout << "-1\n";
+            }
+            else {
+                cout << dq.front() << "\n";
+                dq.pop_front();
+            }
+            
+        }
+        else if(s.compare("size")==0) {
+            cout << dq.size() << "\n";
+        }
+        else if(s.compare("empty") == 0) {
+            cout << dq.empty() << "\n"; 
+        }
+        else if(s.compare("size")==0) {
+            cout << dq.size() << "\n";
+        }
+        else if(s.compare("front")==0) {
+            if(dq.empty()==1) {
+                cout << "-1\n";
+            }
+            else {
+                cout << dq.front() << "\n";
+            }
+            
+        }
+        else if(s.compare("back")==0) {
+            if(dq.empty()==1) {
+                cout << "-1\n";
+            }
+            else {
+                cout << dq.back() << "\n";
+            }
+            
+        }
+    }
+
+    return 0;
+}
