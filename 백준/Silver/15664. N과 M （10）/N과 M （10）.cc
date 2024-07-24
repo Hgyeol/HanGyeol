3247495ca93b1690057859d9da4a87535c3a3284
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-07-24 15:52:00 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,47 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool visited[8];
+int arr[8]={0};
+int result[8];
+int n, m;
+
+void dfs(int cnt) {
+    if(cnt == m) {
+        int flag = 0;
+        for(int i = 0; i < m-1; i++) {
+            if(result[i] > result[i+1]) {
+                flag=1;
+                break;
+            }
+        }
+        if(flag == 0) {
+            for(int i = 0; i < m; i++) {
+                cout << result[i] << " ";
+            }
+            cout << "\n";
+        }
+        return;
+    }
+    int check = 0;
+    for(int i = 0; i < n; i++) {
+        if(visited[i] == false && check != arr[i]) {
+            result[cnt] = arr[i];
+            check = arr[i];
+            visited[i] = true;
+            dfs(cnt+1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr+n);
+    dfs(0);
+    return 0;
+}
