86e7f55628a0a27a316fc950bcbcc28f07437a40
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-08-01 02:58:14 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 8 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -10,39 +10,40 @@ int main() {
     cin >> n >> k;
 
     if (n == k) {
-        cout << 0 << "\n1";
+        cout << "0\n1";
         return 0;
     }
 
-    vector<int> dist(100001, -1);
+    vector<int> arr(100001, -1);
     vector<int> count(100001, 0);
 
     queue<int> q;
     q.push(n);
-    dist[n] = 0;
+    arr[n] = 0;
     count[n] = 1;
 
     while (!q.empty()) {
         int node = q.front();
         q.pop();
-        int current_dist = dist[node];
-        int current_count = count[node];
+        int resc = arr[node];
+        int cnt = count[node];
 
-        vector<int> next_positions = {node - 1, node + 1, node * 2};
+        vector<int> n_arr = {node - 1, node + 1, node * 2};
 
-        for (int next : next_positions) {
+        for (int next : n_arr) {
             if (next >= 0 && next <= 100000) {
-                if (dist[next] == -1) {
-                    dist[next] = current_dist + 1;
-                    count[next] = current_count;
+                if (arr[next] == -1) {
+                    arr[next] = resc + 1;
+                    count[next] = cnt;
                     q.push(next);
-                } else if (dist[next] == current_dist + 1) {
-                    count[next] += current_count;
+                } 
+                else if (arr[next] == resc + 1) {
+                    count[next] += cnt;
                 }
             }
         }
     }
 
-    cout << dist[k] << "\n" << count[k];
+    cout << arr[k] << "\n" << count[k];
     return 0;
 }
