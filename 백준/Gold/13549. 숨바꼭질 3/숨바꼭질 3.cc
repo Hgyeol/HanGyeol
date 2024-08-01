3ac7e5d3dbac95847c6cb15abb910caf1f624498
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-08-01 13:04:57 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -9,7 +9,7 @@ int main() {
     int n, k;
     
     cin >> n >> k;
-    int visited[200001]={0};
+    int visited[100001]={0};
     
     queue<int> q;
     q.push(n);
@@ -21,15 +21,15 @@ int main() {
         //cout<<"node : " << node <<"\n";
         q.pop();
         if(node!=k) {
-            if(node*2 >= 0 && node*2 < 200001 && visited[node*2] == 0) {
+            if(node*2 >= 0 && node*2 < 100001 && visited[node*2] == 0) {
                 q.push(node*2);
                 visited[node*2] = visited[node];
             }
-            if(node-1 >= 0 && node-1 < 200001 && visited[node-1] == 0) {
+            if(node-1 >= 0 && node-1 < 100001 && visited[node-1] == 0) {
                 q.push(node-1);
                 visited[node-1] = visited[node]+1;
             }
-            if(node+1 >= 0 && node+1 < 200001 && visited[node+1] == 0) {
+            if(node+1 >= 0 && node+1 < 100001 && visited[node+1] == 0) {
                 q.push(node+1);
                 visited[node+1] = visited[node]+1;
             }
