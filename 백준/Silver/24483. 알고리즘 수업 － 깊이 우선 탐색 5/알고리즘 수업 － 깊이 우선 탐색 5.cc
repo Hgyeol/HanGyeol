62867e160969d61d7d51752ee9f8546744b9120c
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-08-01 23:34:34 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 5, Time: 244 ms, Memory: 14704 KB -BaekjoonHub

---
+++ 백준/Silver/24483. 알고리즘 수업 － 깊이 우선 탐색 5/알고리즘 수업 － 깊이 우선 탐색 5.cc
@@ -0,0 +1,49 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int result[100001]={0};
+int cnt=1;
+void dfs(int bef, int node, vector<int> DFS[], long long int visited[]) {
+    visited[node] = visited[bef] + 1;
+    //cout << node << " ";
+    result[node] = cnt;
+    cnt++;
+    for(int i = 0; i < DFS[node].size(); i++) {
+        int next = DFS[node][i];
+        if(visited[next] == -1) {
+            dfs(node, next, DFS, visited);
+        }
+    }
+}
+
+int main() {
+    
+    int n, m, r, node, edge;
+    
+    cin >> n >> m >> r;
+    
+    vector<int> DFS[n+1];
+    long long int visited[n+1];
+    long long int sum = 0;
+    fill(visited, visited+n+1, -1);
+    
+    for(int i = 0; i < m; i++) {
+        cin >> node >> edge;
+        
+        DFS[node].push_back(edge);
+        DFS[edge].push_back(node);
+    }
+    
+    for(int i = 1; i <= n; i++) {
+        sort(DFS[i].begin(), DFS[i].end());
+    }
+    dfs(0, r, DFS, visited);
+    for(int i = 1; i <= n; i++) {
+        sum += visited[i] * result[i];
+    }
+    cout << sum;
+    return 0;
+}
