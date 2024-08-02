8fc0544d8ecde232c040fecf553cb3413e2f0fe4
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-08-02 13:52:24 +0900

    [Gold IV] Title: 최단경로, Time: 112 ms, Memory: 9060 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,45 +1,46 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <queue>
-#include <cstring>
 #include <vector>
+#define INF 987654321
 using namespace std;
-int INF = 98765432; 
-int dp[20003];
-vector<pair<int, int>> v[20003];
-void fc(int st){
-    memset(dp, INF, sizeof(dp));
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int v, e, k, a,b,c;
+    int dist[20010];
+    vector<pair<int,int>> vec[20010];
+    cin >> v >> e >> k;
+    for(int i = 0; i < e; i++) {
+        cin >> a >> b >> c;
+        vec[a].push_back(make_pair(b,c));
+    }
+    for(int i = 1; i <= v; i++) dist[i] = INF;
+    
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0, st});
-    dp[st] = 0;
-    while(!pq.empty()){
-        int x = pq.top().second;
+    
+    pq.push(make_pair(0,k));
+    dist[k]=0;
+    
+    while(!pq.empty()) {
+        int node = pq.top().second;
         int cost = pq.top().first;
         pq.pop();
-        for (int i = 0; i < v[x].size();i++){
-            int nx = v[x][i].first;
-            int ncost = cost + v[x][i].second;
- 
-            if(dp[nx] > ncost){
-                pq.push({ncost, nx}); 
-                dp[nx] = ncost;
+        for(int i = 0; i < vec[node].size(); i++) {
+            int next = vec[node][i].first;
+            int ncost = vec[node][i].second;
+            
+            if(dist[next] > cost + ncost) {
+                dist[next] = cost + ncost;
+                pq.push(make_pair(dist[next], next));
             }
         }
     }
-}
-int main(){
-    int V, E, K;
-    cin >> V >> E >> K;
-    int a1, a2, a3;
-    for (int i = 0; i < E;i++){
-        cin >> a1 >> a2 >> a3;
-        v[a1].push_back({a2, a3});
-    }
-    fc(K);
-    for (int i = 1; i <= V;i++){
-        if(dp[i] < INF)
-            cout << dp[i] << '\n';
-        else
-            cout << "INF" << '\n';
+    for(int i = 1; i <= v; i++) {
+        if(dist[i]==INF) cout << "INF\n";
+        else cout << dist[i] << "\n";
     }
     return 0;
 }
