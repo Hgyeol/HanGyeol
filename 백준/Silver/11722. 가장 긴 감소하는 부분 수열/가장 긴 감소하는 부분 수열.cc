494a0a05aa9d96005b6fe76870faa6d48ff29aa6
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-08-19 10:37:00 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,27 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n, max, res=0;
+    cin >> n;
+    int arr[n+1] = {0};
+    int dp[n+1] = {0};
+    for(int i = 1; i <= n; i++) cin >> arr[i];
+    
+    for(int i = 1; i <= n; i++) {
+        max=-1;
+        for(int j = 0; j < i; j++) {
+            if(arr[i] < arr[j]) {
+                if(max < dp[j]) {
+                    max = dp[j];
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+    }
+    for(int i = 0; i <= n; i++) {
+        if(res < dp[i]) res = dp[i];
+    }
+    cout << res+1;
+    return 0;
+}
