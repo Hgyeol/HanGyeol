498135a3d9b0438f184103942dde73ecc4f7eec0
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-09-02 08:12:48 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.cc
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n, num, cnt=0;
+    cin >> n;
+    for(int i = 0; i < 5; i++) {
+        cin >> num;
+        if(num%10==n) cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
