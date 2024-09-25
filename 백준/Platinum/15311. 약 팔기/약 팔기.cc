1104561386d942a130f974e7930d477ffe3c29cc
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-09-26 07:10:06 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    cout << "2000\n";
+    for(int i = 1; i <= 1000; i++) cout << 1 << " ";
+    for(int i = 1001; i <= 2000; i++) cout << 1000 << " ";
+    return 0;
+}
