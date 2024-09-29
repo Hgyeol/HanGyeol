e28e3d921fcbc16ba1912c0d97ff1fee507b223c
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-09-29 23:21:47 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+        cout << a+b << "\n";
+    }
+    return 0;
+}
