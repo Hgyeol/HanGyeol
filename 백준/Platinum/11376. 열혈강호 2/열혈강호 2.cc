54e2fd4023b1c55621ded920bd1ae4c9076385c4
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-10-02 11:34:58 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 3588 ms, Memory: 5988 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.cc
@@ -0,0 +1,64 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> v(1001);
+int sel[1001];
+bool vis[1001][2];
+
+bool bimatch(int n) {
+    if(vis[n][0]) {
+        if(vis[n][1]) return false;
+        else {
+            vis[n][1] = true;
+            for(int i = 0; i < v[n].size(); i++) {
+                int num = v[n][i];
+                if(sel[num] == -1 || bimatch(sel[num])) {
+                    sel[num] = n;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    else {
+        if(vis[n][0]) return false;
+        
+        vis[n][0] = true;
+        
+        for(int i = 0; i < v[n].size(); i++) {
+            int num = v[n][i];
+            if(sel[num] == -1 || bimatch(sel[num])) {
+                sel[num] = n;
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+int main() {
+    int n, m, s, num, cnt=0;
+    cin >> n >> m;
+    fill(sel, sel+1001, -1);
+    for(int i = 1; i <= n; i++) {
+        cin >> s;
+        while(s--) {
+            cin >> num;
+            v[i].push_back(num);
+        }
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < 2; j++) {
+            fill(&vis[0][0], &vis[0][0] + 1001 * 2, false);
+            bimatch(i);
+        }
+    }
+    for(int i = 1; i <= m; i++) {
+        if(sel[i]!=-1) cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
