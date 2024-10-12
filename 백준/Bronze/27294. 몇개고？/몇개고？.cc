c77f3eeec3136e9fe919c89acb2b5f0a635d4b3c
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-10-12 22:29:21 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int t, a;
+    cin >> t >> a;
+    if(a==0 && t >= 12 && t <= 16) cout << 320;
+    else cout << 280;
+    return 0;
+}
