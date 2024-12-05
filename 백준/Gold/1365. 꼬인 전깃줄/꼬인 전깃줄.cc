551ba88367ac42e92eae2e901d45e3ea7799464d
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-12-06 08:12:52 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 36 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int bs(int* dp, int n, int f) {
+    int s = 0;
+    int e = n - 1;
+    while (s < e) {
+        int mid = (s + e) / 2;
+        if (f <= dp[mid]) e = mid;
+        else s = mid + 1;
+    }
+    return s;
+}
+int main() {
+    int n, num;
+    cin >> n;
+    int dp[100001] = {0};
+    int idx=0;
+    for(int i = 1; i <= n; i++) {
+        cin >> num;
+        if(idx==0) dp[idx++]=num;
+        if(dp[idx-1] < num) dp[idx++] = num;
+        else dp[bs(dp, idx, num)] = num;
+        
+    }
+    cout << n-idx;
+    return 0;
+}
