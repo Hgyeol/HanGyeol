2ca7f7382d15483d57b5c779b7c7bf5b081e68ec
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2024-12-06 08:11:05 +0900

    [Gold II] Title: 반도체 설계, Time: 16 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int bs(int* dp, int n, int f) {
+    int s = 0;
+    int e = n - 1;
+    while (s < e) {
+        int mid = (s + e) / 2;
+        if (f <= dp[mid]) e = mid;
+        else s = mid + 1;
+    }
+    return s;
+}
+int main() {
+    int n, num;
+    cin >> n;
+    int dp[40001] = {0};
+    int idx=0;
+    for(int i = 1; i <= n; i++) {
+        cin >> num;
+        if(idx==0) dp[idx++]=num;
+        if(dp[idx-1] < num) dp[idx++] = num;
+        else {
+            dp[bs(dp, idx, num)] = num;
+        }
+        
+    }
+    cout << idx;
+    return 0;
+}
