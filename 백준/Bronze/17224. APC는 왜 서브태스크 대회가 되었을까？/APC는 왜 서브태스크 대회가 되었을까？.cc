eddcbc25b8e40727a1aa41e28f6c3b8615de1109
Hgyeol <140942644+Hgyeol@users.noreply.github.com>
2025-01-02 09:14:36 +0900

    [Bronze I] Title: APC는 왜 서브태스크 대회가 되었을까?, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/17224. APC는 왜 서브태스크 대회가 되었을까？/APC는 왜 서브태스크 대회가 되었을까？.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n,l,k, num1, num2;
+    vector<int> v;
+    cin >> n >> l >> k;
+    for(int i = 0; i < n; i++) {
+        cin >> num1 >> num2;
+        if(num2<=l) v.push_back(2);
+        else if(num1<=l) v.push_back(1);
+    }
+    sort(v.rbegin(), v.rend());
+    int res=0;
+    for(int i = 0; i < v.size(); i++) {
+        if(v[i]==2) res += 140;
+        else res += 100;
+        if(i==k-1) break;
+    }
+    cout << res;
+    return 0;
+}
